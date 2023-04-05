94c77534432429f16a64b413d3cc45295d5fdfd0
vinoth2918 <yemvinoth92@gmail.com>
2023-04-05 19:10:36 +0530

    First Time Push

---
+++ src/main/java/com/sample/GITClass/ClassA.java
@@ -0,0 +1,16 @@
+package com.sample.GITClass;
+
+public class ClassA {
+	
+	private void method1() {
+		System.out.println("1");
+	
+	}
+      private void method2() {
+    	  System.out.println("1");
+    	  
+      }
+      private void method3() {
+    	  System.out.println("3");
+      }
+}
